49c72501538e5be595b231f9703403b257603460
Gabriel Villegas <nspooky@outlook.com>
2020-03-21 16:14:24 -0600

    Add project files.

---
+++ curso de csharp core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso_de_csharp_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
